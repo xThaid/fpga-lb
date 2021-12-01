f7c3fce4fc0502172647401528581b2e3e24b2f7
Jakub Urbańczyk <xthaid@gmail.com>
2021-12-01 14:18:36 +0100

    [proj] Small file reorganization

--- proj/quartus/qsys/components/lb_dataplane/comp.mk
+++ proj/quartus/qsys/components/lb_dataplane/comp.mk
@@ -3,4 +3,4 @@ HWCOMP-TOPLEVEL := lb_dataplane
 HWCOMP-SOURCES += rtl/lb_dataplane/lb_dataplane.v
 HWCOMP-SOURCES += rtl/lb_dataplane/arp_cache.v
 
-HWCOMP-SOURCES += ip/crc32.v
+HWCOMP-SOURCES += ip/crc/crc32.v
