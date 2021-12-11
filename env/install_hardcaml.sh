c3ccf2f44d9eff4979e99df2bde95d55868ff1b6
Jakub Urbańczyk <xthaid@gmail.com>
2021-12-11 14:18:12 +0100

    Add hardcaml to the dev env

---
+++ env/install_hardcaml.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -x
+
+OCAML_VERSION=4.12.1
+OPAM_VERSION=2.0.5
+
+apt-get install -y unzip build-essential gcc curl coreutils libgmp-dev
+
+curl -L -o /usr/bin/opam "https://github.com/ocaml/opam/releases/download/$OPAM_VERSION/opam-$OPAM_VERSION-$(uname -m)-$(uname -s)" && \
+    chmod 755 /usr/bin/opam
+
+
+su user -c "opam init -y --comp $OCAML_VERSION --disable-sandboxing"
+
+su user -c "opam repo add janestreet-bleeding https://ocaml.janestreet.com/opam-repository"
+su user -c "opam install -y utop hardcaml hardcaml_waveterm ppx_deriving_hardcaml dune hardcaml_step_testbench"
+
+su user -c "echo 'test -r /home/user/.opam/opam-init/init.sh && . /home/user/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true' >> ~/.bashrc"
