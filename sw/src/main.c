3f0e1d842e0b80d7b63624f61c4a228931a7c50a
Jakub Urbańczyk <xthaid@gmail.com>
2021-12-11 23:01:28 +0100

    Base for the soft-core program

---
+++ sw/src/main.c
@@ -0,0 +1,117 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+#define DELAY_LOOP 10000
+#define TRUE 1
+
+void printf(char *c) {
+
+}
+
+void task1 (void *pvParameters) {
+
+	while(TRUE) {
+
+		printf("Task 1\n");
+
+		taskYIELD();
+
+		for(int i = 0; i < DELAY_LOOP; i++)
+			portNOP();
+	}
+
+	vTaskDelete(NULL);
+
+}
+
+void task2 (void *pvParameters) {
+
+	while(TRUE) {
+
+		printf("Task 2\n");
+
+		taskYIELD();
+
+		for(int i = 0; i < DELAY_LOOP; i++)
+			portNOP();
+	}
+
+	vTaskDelete(NULL);
+}
+
+int main( void )
+{
+	printf("Starting FreeRTOS\n");
+	xTaskCreate(task1, "Task 1", 100, NULL, 1, NULL);
+ 	xTaskCreate(task2, "Task 2", 100, NULL, 1, NULL);
+	vTaskStartScheduler();
+
+	return 0;
+}
+
+void vApplicationMallocFailedHook( void )
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook( void )
+{
+}
+/*-----------------------------------------------------------*/
+
+void vAssertCalled( void )
+{
+volatile uint32_t ulSetTo1ToExitFunction = 0;
+
+	taskDISABLE_INTERRUPTS();
+	while( ulSetTo1ToExitFunction != 1 )
+	{
+		__asm volatile( "NOP" );
+	}
+}
+
+void handle_trap(void)
+{
+	while (1)
+		;
+}
