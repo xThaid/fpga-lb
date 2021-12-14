8c110e853fdf5e427671b62a269ad95605b2ccdb
Jakub Urbańczyk <xthaid@gmail.com>
2021-12-14 12:09:29 +0100

    Add OpenOCD to the dev env

---
+++ env/install_openocd_vex.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -x
+
+apt-get install -y libtool automake libusb-1.0-0-dev texinfo libusb-dev libyaml-dev pkg-config
+git clone https://github.com/SpinalHDL/openocd_riscv
+cd openocd_riscv
+./bootstrap
+./configure --prefix=/opt/openocd_vex --enable-ftdi --enable-dummy
+make -j $(nproc)
+make install
