3b78ea54cbd466475b8c04fb4b2866f8ff08a650
Jakub Urbańczyk <xthaid@gmail.com>
2021-12-14 19:50:32 +0100

    Decrease available amount of memory

--- sw/include/FreeRTOSConfig.h
+++ sw/include/FreeRTOSConfig.h
@@ -51,7 +51,7 @@
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 7 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 512 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) 64500 )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) 8000 )
 #define configMAX_TASK_NAME_LEN			( 16 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
